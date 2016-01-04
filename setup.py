6e87d7273c12209a521dc02e0bb1986ddbc26b34
Christian Schwede <info@cschwede.de>
2016-01-04 11:24:26 +0100

    Version 0.21 release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-swiftbrowser',
-    version='0.2',
+    version='0.21',
     packages=['swiftbrowser'],
     include_package_data=True,
     license='Apache License (2.0)',
